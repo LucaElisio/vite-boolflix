6d783c88052c9293ef0d79b2b555ba77c4f20ca3
LucaElisio <personal.elix@gmail.com>
2024-05-08 12:57:09 +0200

    feat: api call on page load

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <script>
 import axios from 'axios';
 import { store } from './store';
+import AppMain from './components/AppMain.vue';
 
 export default {
+  components: { AppMain },
   data() {
     return {
       store,
     }
   },
   created(){
-    axios.get("https://api.themoviedb.org/3/search/movie", {
+
+    // Chiamata per i movies popolari
+    axios.get("https://api.themoviedb.org/3/movie/popular", {
       params: {
         api_key: this.store.apiKey,
       }
     }).then((resp) => {
-      console.log(resp);
+      // console.log(resp);
+      // Salvo l'array di risultati nello store
+      this.store.moviesArray = resp.data.results;
+      console.log(this.store.moviesArray);
     })
   }
 }
 </script>
 
 <template>
-  <h1>CIAO</h1>
+  <AppMain />
 </template>
 
 <style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<script>
+import { store } from '../store'
+
+
+export default {
+    data(){
+        return{
+            store
+        }
+    },
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="curMovie in this.store.moviesArray">
+            <p>Titolo: {{curMovie.title}}</p>
+            <p>Titolo originale: {{curMovie.original_title}}</p>
+            <p>Lingua originale: {{curMovie.original_language}}</p>
+            <p>Voto: {{curMovie.vote_average}}</p>
+        </li>
+    </ul>
+</template>
+
+<style scoped lang="scss">
+
+</style>
