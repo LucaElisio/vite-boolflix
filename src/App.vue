a49d3eae5863deb5becd10b76131c2c9f5cfcbdd
LucaElisio <personal.elix@gmail.com>
2024-05-08 12:24:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios';
+import { store } from './store';
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  created(){
+    axios.get("https://api.themoviedb.org/3/search/movie", {
+      params: {
+        api_key: this.store.apiKey,
+      }
+    }).then((resp) => {
+      console.log(resp);
+    })
+  }
+}
+</script>
+
+<template>
+  <h1>CIAO</h1>
+</template>
+
+<style>
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import {reactive} from "vue";
+
+export const store = reactive ({
+    moviesArray: [],
+    tvArray: [],
+    apiKey: "a715b03eddf08f3fd00576d89c1b3e92",
+    searchQuery: "",
+})
