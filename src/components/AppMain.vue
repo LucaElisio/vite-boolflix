e6655bf28d9ca86e25a856aaeca57d3ccb9c3a6a
LucaElisio <personal.elix@gmail.com>
2024-05-08 15:00:46 +0200

    feat: Transform static language string into corresponding national flag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,6 +8,28 @@ export default {
             store
         }
     },
+    methods: {
+        showFlag(curFlag){
+            let imgUrl = ""
+            switch(curFlag){
+                case "en":
+                    imgUrl = "../src/assets/img/united-kingdom.png"
+                    break;
+                case "it":
+                    imgUrl = "../src/assets/img/italy.png"
+                    break; 
+                case "fr":
+                    imgUrl = "../src/assets/img/france.png"
+                    break; 
+                case "ja":
+                    imgUrl = "../src/assets/img/japan.png"
+                    break;
+                default:
+                    return "Bandiera non disponibile"   
+            }
+            return imgUrl
+        }
+    }
 }
 </script>
 
@@ -17,7 +39,9 @@ export default {
             <p>Titolo: {{curMovie.title}}</p>
             <p>Titolo originale: {{curMovie.original_title}}</p>
             <p>Lingua originale: {{curMovie.original_language}}</p>
-            <p>Voto: {{curMovie.vote_average}}</p>
+            <p>Voto: {{curMovie.vote_average}}</p>           
+            <img :src="showFlag(curMovie.original_language)" alt="" v-if="showFlag(curMovie.original_language) != 'Bandiera non disponibile'">
+            <p v-else>{{showFlag(curMovie.original_language)}}</p>
         </li>
     </ul>
 </template>
